a85444eb965d793dcea2a0e60f54b92d2d822418
SURESH-S-U <suresh.elite07@gmail.com>
2025-06-30 15:22:27 +0530

    Pattern printing program Added

---
+++ Boiler_Plate_Code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Boiler_Plate_Code{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        
+        String str = sc.nextLine();
+
+    }
+
+}
