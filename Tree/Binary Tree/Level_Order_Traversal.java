c85510b4b0449469339d0292b8e49bf3304978ab
SURESH-S-U <suresh.elite07@gmail.com>
2025-07-15 20:44:09 +0530

    level Order program

---
+++ Tree/Binary Tree/Level_Order_Traversal.java
@@ -0,0 +1,39 @@
+class Node {
+    int data;
+    Node left, right;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Main {
+    public static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();  // removes front
+            System.out.print(current.data + " ");
+
+            if (current.left != null)
+                queue.add(current.left);
+            if (current.right != null)
+                queue.add(current.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        levelOrder(root); // Output: 1 2 3 4 5 6 7
+    }
+}
